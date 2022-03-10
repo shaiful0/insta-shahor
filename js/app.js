1562f00e123478cc04cd315c7a8ce77806891e13
sharifulislam <shariful0401@gmail.com>
2022-03-11 01:17:27 +0600

    update

--- js/app.js
+++ js/app.js
@@ -63,7 +63,7 @@ const createPost = (post) => {
                     target="_blank"
                     class="post__avatar"
                   >
-                    <img src="${post.Image}" alt="User Picture" />
+                    <img src="${post.userImage}" alt="User Picture" />
                   </a>
                   <a href="#" class="post__user">phero</a>
                 </div>
